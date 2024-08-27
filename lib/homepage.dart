55de0e1d0065b1ab95edaef17cb47e7b5ee9dbff
Ayushi Pandey <ayushipandey20903@gmail.com>
2024-08-27 19:49:43 +0530

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,6 +7,7 @@ class PasswordGeneratorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: PasswordGeneratorScreen(),
     );
   }
